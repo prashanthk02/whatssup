908d2836ef479b29e8f95f1f72bc58e2cb00b8cc
prashanthk02 <prashanth.k369@gmail.com>
2022-05-13 19:13:12 -0400

    Add api call from backend

--- backend/routes/search_recipes.js
+++ backend/routes/search_recipes.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
+const axios = require("axios");
+const { response } = require('express');
 
 const users = [];
 
+const searchByIngredients = (req, res) => {
+  const ingredients = 'potato, mushroom'//req.params;
+  axios
+    .get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.apiKey}&ingredients=${ingredients}&number=6`)
+    .then((response) => res.send(response.data))
+}
+
 module.exports = (db) => {
-  router.get('/', (req, res) => {
-    res.json(users);
-  });
-
-  router.post('/', (req,res) => {
-
-  })
+  router.get('/', searchByIngredients)
+  // router.get('/', (req, res) => {
+  //   res.json(searchByIngredients());
+  // });
   return router;
 }
