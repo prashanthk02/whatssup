7de9557ebc9385a95ddfdbf7dd901ebc2afc38a5
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-13 09:31:21 -0400

    set up server port

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const port = 3001
 
 // db connection
 const db = require('./configs/db.config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,5 +21,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter());
 
-
+app.listen(port, console.log(`serve listening to port ${port}`))
 module.exports = app;
