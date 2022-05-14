abab9ac27966d8a7bbe29f44be129d128abf7c01
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-13 20:26:57 -0400

    define helper function to query database for input registration email

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -2,6 +2,18 @@ const router = require('express').Router();
 
 const users = ['Bob', 'Will'];
 
+const getUserByEmail = function(email) {
+  const queryString = `SELECT *
+  FROM users
+  WHERE email = $1
+  `;
+  return db.query(queryString, [email])
+    .then((result) => {
+      return result.rows[0];
+    });
+};
+
+
 module.exports = (db) => {
   router.get('/', (req, res) => {
     res.json(users);
