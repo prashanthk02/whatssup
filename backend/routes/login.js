50a9f065afba24f081df6e2b5f9ef52aa9c96e44
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-13 20:08:55 -0400

    define a helper function to query database for existing user and password

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 
-const users = ['Bob', 'Will', 'Tristan'];
+//helper function to query database.
+const getUserByEmail = function (email, password, db) {
+  const queryStringEmail = `SELECT *
+  FROM users
+  WHERE email = $1
+  AND password = $2`
+  const values = [email, password]
+  return db
+    .query(queryStringEmail, values)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
@@ -8,8 +23,8 @@ module.exports = (db) => {
   });
 
   router.post('/', (req, res) => {
-  
+
   });
-  
+
   return router;
 }
