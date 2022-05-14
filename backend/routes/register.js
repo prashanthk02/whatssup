026cead6044d969c2a060d9a6e7028bf6391920f
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-13 21:04:25 -0400

    fix bug

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -23,6 +23,7 @@ $1, $2, $3) RETURNING *`;
 };
 
 module.exports = (db) => {
+  
   router.get('/', (req, res) => {
     res.json(users);
   });
@@ -33,6 +34,22 @@ module.exports = (db) => {
     if (user.email === "" || user.password === "" || user.name === "") {
       return res.status(403).send("Invalid User");
     }
+    getEmail(email)
+      .then(checkemail => {
+        if (checkemail) {
+          res.status(400).send('Email already exists');
+        }
+        else {
+          addUser(user.name, user.email, user.password) // add new user to database
+            .then(result => {
+              if (!result) {
+                return res.status(400).send({ error: "error" });
+              }
+              const userID = result.rows[0];
+              req.session.userID = userID.id;
+            });
+        }
+      });
   })
   return router;
 }
