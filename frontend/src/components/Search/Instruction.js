56bb81ea8791e0aa38023c738979ed89f37a0da0
jchanpark <junpark4936@gamil.com>
2022-05-14 19:54:54 -0400

    add components instruction

---
+++ frontend/src/components/Search/Instruction.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import SearchResultsListItem from "./SearchResultsListItem";
+
+export default function Instruction (prop) {
+
+  const {} = props
+  
+  return (
+    
+    <div className="instruction">
+      <h5>Instructions</h5>
+      <p>${}</p>      
+
+
+    </div>
+
+  )
+}
