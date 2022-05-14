a546bf03d5bd6580aa7bcf9ab8933984b068dbc8
prashanthk02 <prashanth.k369@gmail.com>
2022-05-13 21:15:28 -0400

    Add components

---
+++ frontend/src/components/Search/SearchResultsListItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function SearchResultsListItem(props) {
+  const {} = props
+  return (
+    <div>
+      <img 
+        className="results--img"
+        src=''
+        alt=''
+      />
+      <span>{title}</span>
+    </div>
+  );
+}
