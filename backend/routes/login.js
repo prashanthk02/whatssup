78f6d76261bdf0ab46053cd82662b1fbd8f378af
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-14 15:46:10 -0400

    refactor code

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,25 +1,26 @@
 const router = require('express').Router();
 
+const users = ["login page"];
 
-const users = ["loginpage"];
-
-module.exports = (db) => {
-  //helper function to query database.
-  const getUserByEmail = function (email, password, db) {
-    const queryStringEmail = `SELECT *
+//helper function to query database.
+const getUserByEmail = function (email, password, db) {
+  const queryStringEmail = `SELECT *
   FROM users
   WHERE email = $1
   AND password = $2`
-    const values = [email, password]
-    return db
-      .query(queryStringEmail, values)
-      .then((result) => {
-        console.log(result.rows[0]);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }
+  const values = [email, password]
+  return db
+    .query(queryStringEmail, values)
+    .then((result) => {
+      console.log(result.rows[0]);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+
+module.exports = (db) => {
+
   router.get('/', (req, res) => {
     getUserByEmail('nhinrichsen0@ezinearticles.com', '2C0?1nvP!B', db);
     res.json(users);
