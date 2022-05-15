c9c5cc4f47a8b17d956fa0e39a2f932b9cccbfa9
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-15 00:53:43 -0400

    define login State variables

--- frontend/src/components/Navbar/Login.js
+++ frontend/src/components/Navbar/Login.js
@@ -1,17 +1,25 @@
+import { useState } from "react"
+
 export default function Login() {
+  const [email,  setEmail] = useState("")
+  const [password,  setPassword] = useState("")
+
+  
   return (
     <>
-      <form action="/login">
+      <form onSubmit={onSubmitLoginForm}>
         <input
           type="text"
           className="form-control"
           value={email}
+          placeholder="enter email"
           onChange={e => setEmail(e.target.value)}
         />
         <input
           type="text"
           className="form-control"
-          value={email}
+          value={password}
+          placeholder="enter password"
           onChange={e => setPassword(e.target.value)}
         />
         <button id="login">Login</button>
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "../../styles/navbar.scss"
+import Login from "./Login";
 
 export default function Navbar() {
   return (
     <div className="nav">
       <h2 className="nav--title"> What's Supp? </h2>
+      <Login />
     </div>
   );
 }
