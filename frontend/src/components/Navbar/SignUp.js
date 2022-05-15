6baedff32a94aa935f8759bf5e1a112c50c360de
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-15 01:33:10 -0400

    create signup state variables

--- frontend/src/components/Navbar/SignUp.js
+++ frontend/src/components/Navbar/SignUp.js
@@ -1,6 +1,9 @@
 import { useState } from "react"
 
 export default function SignUp() {
+  const [email,  setEmail] = useState("")
+  const [password,  setPassword] = useState("")
+  const [name, setName] = useState("")
 
   return (
     <>
