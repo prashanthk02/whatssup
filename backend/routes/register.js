7b5ecb856df821834431ff52a8cff64539bd0862
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-15 03:54:55 -0400

    fix conflit with merge

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -24,25 +24,6 @@ $1, $2, $3) RETURNING *`;
     });
 };
 
-//define helper function to query database for input registration email
-const getUserByEmail = function(email) {
-  const queryString = `SELECT *
-  FROM users
-  WHERE email = $1
-  `;
-  return db.query(queryString, [email])
-    .then((result) => {
-      return result.rows[0];
-    });
-};
-
-//define a helper function to add new user to database
-const addUser = function(name, email, password) {
-  const queryString = `INSERT INTO users (name, email, password)
-VALUES(
-$1, $2, $3) RETURNING *`;
-  return db.query(queryString, [name, email, password]);
-};
 
 module.exports = (db) => {
   
