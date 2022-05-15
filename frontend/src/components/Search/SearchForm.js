f943fd4ae60d82d3b4490e665d75b04ce871edc4
jchanpark <junpark4936@gamil.com>
2022-05-15 12:22:15 -0400

    refactor seeds/recipes

--- frontend/src/components/Search/SearchForm.js
+++ frontend/src/components/Search/SearchForm.js
@@ -1,17 +1,34 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "../../styles/search.scss"
 
 export default function Search() {
-  const [search, setSearch] = useState("")
+  const [ingredients, setIngredients] = useState("")
+  const [recipe, setRecipe] = useState(null)
+
+  function handleChange(e) {
+    setIngredients(e.target.value);
+  }
+
+  function getByIngredients() {
+    axios
+      .get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&ingredients=${ingredients}`)
+      .then((response) => {
+        const result = response.data.results;
+        setRecipe({result});
+      })
+      .catch(() => {console.log('err')});
+  }
+
   return (
     <div className="search--box">
 
       <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
         <label>What you have: </label>
-        <input type="text" className="search" placeholder="potato,tomato" />
+        <input type="text" className="search" placeholder="potato,tomato" onChange={handleChange} />
       </form>
-      <button type="button" className="search--btn">Find Recipe</button>
+      <button type="button" className="search--btn" onClick={getByIngredients}>Find Recipe</button>
 
     </div>
   );
