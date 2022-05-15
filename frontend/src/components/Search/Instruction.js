6bd4bf579f89bc115f4e71195e4336ffb10ca4bf
jchanpark <junpark4936@gamil.com>
2022-05-15 11:02:21 -0400

    add component instruction

---
+++ frontend/src/components/Search/Instruction.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+
+import SearchResultsListItem from "./SearchResultsListItem";
+
+export default function Instruction (prop) {
+
+  const {} = prop
+  
+  return (
+    
+    <fragment>
+      <h5 className="text-left" >Instructions</h5>
+      <p>${}</p>      
+
+
+    </fragment>
+
+  )
+}
