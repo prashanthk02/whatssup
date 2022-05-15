87b070b2e74c5b9577c65a055dbd4b65f56b89e8
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-15 00:58:51 -0400

    define onSubmit helpfer function for fetch request

--- frontend/src/components/Navbar/Login.js
+++ frontend/src/components/Navbar/Login.js
@@ -4,7 +4,22 @@ export default function Login() {
   const [email,  setEmail] = useState("")
   const [password,  setPassword] = useState("")
 
-  
+  const onSubmitLoginForm = async e => {
+    e.preventDefault();
+    try {
+      const user = { email, password };
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user)
+      });
+      console.log(response)
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <>
       <form onSubmit={onSubmitLoginForm}>
