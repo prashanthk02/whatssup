b15b94841c4eb734147caf1e90f0a38249ed1d0b
jchanpark <junpark4936@gamil.com>
2022-05-16 02:30:42 -0400

    refactor search-ingredients

--- frontend/src/components/Search/SearchResultsList.js
+++ frontend/src/components/Search/SearchResultsList.js
@@ -2,9 +2,16 @@ import React from "react";
 
 import SearchResultsListItem from "./SearchResultsListItem";
 
-export default function SearchResults(props) {
-  const {} = props
+export default function SearchResults(recipe) {
+  console.log('recipe---3', recipe);
+  
   return (
-    <SearchResultsListItem />
+    <section>
+      <h3>List</h3>
+      {/* {recipe.map((r) => {
+        return <SearchResultsListItem key={r.id} singleRecipe={r} />;
+      })} */}
+
+    </section>
   );
 }
