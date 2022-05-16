ade0c38776eece52c40cf1b5001cb5ee93f7afc6
jchanpark <junpark4936@gamil.com>
2022-05-16 17:52:37 -0400

    add component-Layout

--- frontend/src/components/Search/SearchResultsList.js
+++ frontend/src/components/Search/SearchResultsList.js
@@ -2,15 +2,15 @@ import React from "react";
 
 import SearchResultsListItem from "./SearchResultsListItem";
 
-export default function SearchResults(recipe) {
-  console.log('recipe---3', recipe);
+export default function SearchResults({recipe}) {
+  console.log('recipe+++++', recipe);
   
   return (
     <section>
-      <h3>List</h3>
-      {/* {recipe.map((r) => {
+      <h1>List</h1>
+      {recipe.results.map((r) => {
         return <SearchResultsListItem key={r.id} singleRecipe={r} />;
-      })} */}
+      })}
 
     </section>
   );
