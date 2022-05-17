363a1ba1c9c8104b1585f14a7bb1bfb99d13de20
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-17 12:20:00 -0400

    create a helper function to add favorite recipe

--- backend/routes/favorite_recipes.js
+++ backend/routes/favorite_recipes.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 
-module.exports = (db) => {
 
+//helper function to query database.
+const addRecipe = function (user_id, recipe_id, title, image, db) {
+    const queryString = `INSERT INTO favorites(user_id, recipe_id, title, image)
+    VALUES ($1, $2, $3, $4) RETURNING *`;
+
+    return db.query(queryString, [user_id, recipe_id, title, image]);
+  };
+
+module.exports = (db) => {
 
 
   return router;
 }
+
