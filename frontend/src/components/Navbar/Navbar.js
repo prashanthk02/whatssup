9070cb4c4f040eb0358f6e615d2f077f74c75ce9
jchanpark <junpark4936@gamil.com>
2022-05-18 19:32:58 -0400

    fix conflict

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -13,16 +13,10 @@ export default function Navbar() {
       <NavLink className="link" to={'/'}>
         <h2 className="nav--title"> What's Supp? </h2>
       </NavLink>
-<<<<<<< HEAD
         {mode === "" && <button onClick={() => setMode("SignIn")}>Login</button>}
         {mode === "" && <button onClick={() => setMode("SignUp")}>Sign up</button>}
-        {(mode === "SignIn" || mode === "SignUp") && <Login mode={mode} setMode={setMode}/>}
-=======
-      {mode === "" && <button onClick={() => setMode("SignIn")}>Login</button>}
-      {mode === "" && <button onClick={() => setMode("SignUp")}>Sign up</button>}
-      {(mode === "SignIn" || mode === "SignUp") && <Login mode={mode} setMode={setMode} />}
-      {( mode === "SignUp") && <SignUp mode={mode} setMode={setMode} />}
->>>>>>> 09a002d9262a22c999192e3d81b8c2e6d833630d
+        {(mode === "SignIn" || mode === "SignUp") && <Login mode={mode} setMode={setMode}/>}      
+        {(mode === "SignUp") && <SignUp mode={mode} setMode={setMode} />}
     </div>
   );
 }
