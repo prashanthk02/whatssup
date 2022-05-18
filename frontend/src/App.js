8921c4049e21b0ccc0173835d60f3c68221ccd9a
prashanthk02 <prashanth.k369@gmail.com>
2022-05-18 19:13:41 -0400

    remove unused imports in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
-import Search from './components/Search';
 import Footer from './components/Footer/Footer';
 import Pages from './pages/Pages';
-import Category from "./components/Category"
-import MealPlan from './components/MealPlan';
 
 function App() {
   return (
