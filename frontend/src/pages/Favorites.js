76d8ef324d832e5f0854acfc55fb84c03b2d32ac
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-05-19 19:07:17 -0400

    implement delete a recipe control function

--- frontend/src/pages/Favorites.js
+++ frontend/src/pages/Favorites.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from "axios";
-import {userContext} from '../providers/AuthProvider';
 
 
 export default function Cuisine() {
@@ -9,7 +8,6 @@ export default function Cuisine() {
 
 	let params = useParams();
 
-  const { user} = useContext(userContext);
    // get user's favorite recipes control
    const getUserFavorite = async (userId) => {
     if (userId) {
@@ -21,15 +19,14 @@ export default function Cuisine() {
   };
 
 
-  // helper function to handle delete recipe to favorite list.
-  function deleteUserFavorite() {
-    return axios.post(`http://localhost:8080/favorite/{params.name}`, { user_id: user.user_id, id: params.name, title: details.title, image: details.image })
-      .then(() => {
+  // helper function to handle delete a recipe from favorite list.
+  const deleteUserFavorite = function (recipeId) {
+    return axios.delete(`http://localhost:8080/favorite/${recipeId}`)
+      .then((response) => {
+        getUserFavorite(params.id);
       });
   }
   
-
-
 	useEffect(() => {
 		getUserFavorite(params.id);
 	}, [params.id]);
@@ -43,7 +40,7 @@ export default function Cuisine() {
 							<img src={recipe.image} alt={recipe.title} />
 							<h4>{recipe.title}</h4>
 						</Link>
-            <button >
+            <button onClick={() => deleteUserFavorite(recipe.id)} >
 					    Delete from favorites {recipe.id} 
 				    </button>
 					</div>
